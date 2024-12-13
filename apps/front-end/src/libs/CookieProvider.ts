1d4493955c77be62b0dcd7282de02283e30e40ae
Ovie Revaldi <ovierevaldi@gmail.com>
2024-12-13 15:27:56 +0700

    Add encrypt method for cookie user

--- apps/front-end/src/libs/CookieProvider.ts
+++ apps/front-end/src/libs/CookieProvider.ts
@@ -1,16 +1,29 @@
 import Cookies from "js-cookie";
 import { UserCookie } from "@/types/UserData";
+import EncryptProvider from "./Encryption";
 
 const getUserData = () => {
     const userData =  Cookies.get('user_id');
+
     if(userData){
-        return JSON.parse(userData)
+        const decryptedVal = EncryptProvider().decrypt(userData);
+        if(decryptedVal)
+            return JSON.parse(decryptedVal)
+        else{
+            return {error: "Cannot Decrypt Cookie"}
+        }
     }
     else return userData;
 };
 
 const setUserData = (userData: UserCookie) => {
-    return Cookies.set('user_id', JSON.stringify(userData));
+    // Cookie Expired in 7 days
+    const encryptedVal = EncryptProvider().encrypt(JSON.stringify(userData));
+    if(encryptedVal)
+        return Cookies.set('user_id', encryptedVal , {expires: 7, sameSite: 'Lax'});
+    else{
+        return {error: "Cannot Encrypt Cookie"}
+    }
 }
 
 export { getUserData, setUserData }
---
+++ apps/front-end/src/libs/Encryption.ts
@@ -0,0 +1,20 @@
+import CryptoJS, { enc } from 'crypto-js'
+
+const EncryptProvider = () => {
+    
+    const encrypt = (value: string) => {
+        if(process.env.NEXT_PUBLIC_ENCRYPT_KEY)
+            return CryptoJS.AES.encrypt(value, process.env.NEXT_PUBLIC_ENCRYPT_KEY).toString();
+    }
+
+    const decrypt = (encryptedValue: string) => {
+        if(process.env.NEXT_PUBLIC_ENCRYPT_KEY){
+            const decryptedBytes = CryptoJS.AES.decrypt(encryptedValue, process.env.NEXT_PUBLIC_ENCRYPT_KEY);
+            return decryptedBytes.toString(CryptoJS.enc.Utf8);
+        }
+    }
+    
+    return {encrypt, decrypt}
+};
+
+export default EncryptProvider
